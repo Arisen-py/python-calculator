673141b7e403f978c3e9425b541d458033a5d64b
Arisen_Early-Coder <n09824730@gmail.com>
2025-12-15 20:09:05 +0530

    Add files via upload

---
+++ Python Simple Calculatorv2.py
@@ -0,0 +1,51 @@
+import time
+
+print("Welcome to the Calculator!")
+def opr():
+    print("Operations available: ")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+while True:
+    oprt = int(input("Choose what to do(no.): "))
+    if oprt == 1:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number to be added: "))
+        print(f"Result: {a + b}.")
+        while True:
+            ans1 = input("Clear?(y/n) ")
+            if ans1 == "y":
+                opr()
+                break
+            elif ans1 == "n":
+                time.
+        
+    elif oprt == 2:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number to be subtracted: "))
+        print(f"Result: {a - b}.")
+        
+    elif oprt == 3:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number to be multiplied: "))
+        print(f"Result: {a * b}.")
+        
+    elif oprt == 4:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number to be divided: "))
+        if b == 0:
+            print("Cannot divide by 0.")
+            print("Please Retry.")
+        else:
+            print(f"Result: {a / b}.")
+        
+    elif oprt == 5:
+        print("Have a great day!")
+        exit()
+        
+    else:
+        print("Invalid choice. Please retry.")
+       
